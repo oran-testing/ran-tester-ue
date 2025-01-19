f55e2cae0ede61840b67bc8ba0e492ff41563a18
Charles Ueltschey <chaseuelt@gmail.com>
2025-01-18 18:03:47 -0600

    Working on metrics export from srsue

--- srsue/src/metrics_influxdb.cc
+++ srsue/src/metrics_influxdb.cc
@@ -40,7 +40,6 @@ metrics_influxdb::metrics_influxdb(std::string influxdb_url,
                                    std::string influxdb_bucket) :
   influx_server_info(influxdb_url, influxdb_port, influxdb_org, influxdb_token, influxdb_bucket)
 {
-  cout << "Initialized influxdb!\n";
 }
 
 metrics_influxdb::~metrics_influxdb()
@@ -52,17 +51,39 @@ void metrics_influxdb::stop() {}
 
 void metrics_influxdb::set_metrics(const ue_metrics_t& metrics, const uint32_t period_usec)
 {
-  cout << "DEBUG MCS: " << metrics.phy.dl[0].mcs << "\n";
 
+
+  float dl_rate_sum = 0.0, ul_rate_sum = 0.0;
+  for (size_t i = 0; i < metrics.stack.rrc.ues.size(); i++) {
+    dl_rate_sum += metrics.stack.mac.ues[i].tx_brate / (metrics.stack.mac.ues[i].nof_tti * 1e-3);
+    ul_rate_sum += metrics.stack.mac.ues[i].rx_brate / (metrics.stack.mac.ues[i].nof_tti * 1e-3);
+  }
+
+  const srsran::sys_metrics_t& m = metrics.sys;
+
+  // Make POST request to influxdb with all metrics
   std::string response_text;
-  int         result = influxdb_cpp::builder()
+  influxdb_cpp::builder()
                    .meas("ue_info")
+                   .tag("pci", "test")
                    .tag("rnti", "test")
-                   .field("mcs", float_to_string(metrics.phy.dl[0].mcs, 4))
+                   .tag("testbed", "default")
+
+                   .field("nof_ue", metrics.stack.rrc.ues.size())
+                   .field("dl_brate", SRSRAN_MAX(0.0f, (float)dl_rate_sum))
+                   .field("ul_brate", SRSRAN_MAX(0.0f, (float)ul_rate_sum))
+                   .field("proc_rmem", m.process_realmem)
+                   .field("proc_rmem_kB", m.process_realmem_kB)
+                   .field("proc_vmem", m.process_virtualmem)
+                   .field("proc_vmem_kB", m.process_virtualmem_kB)
+                   .field("sys_mem", m.system_mem)
+                   .field("system_load", m.process_cpu_usage)
+
                    .timestamp(get_timestamp())
                    .post_http(influx_server_info, &response_text);
-  cout << "RESP: " << result << "\n";
-  cout << "RESP TEXT: " << response_text << "\n";
+  if(response_text.length > 0){
+    cout << "Recieved error from influxdb: " << response_text << "\n";
+  }
 }
 
 std::string metrics_influxdb::float_to_string(float f, int digits)
