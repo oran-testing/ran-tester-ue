4a25d4f4a4e11617f181b6c3c11f0b55e31d9538
Charles Ueltschey <chaseuelt@gmail.com>
2025-07-21 09:21:25 -0500

    Working on fixing AI integration

--- llm_worker/main.py
+++ llm_worker/main.py
@@ -214,41 +214,45 @@ if __name__ == '__main__':
         logging.error("Model not specified")
         sys.exit(1)
 
-    base_prompt = Config.options.get("base_prompt", None)
-    if not base_prompt:
-        raise RuntimeError(f"base prompt not provided in {Config.filename}")
 
+    # Setup control API info
+    control_url = f"https://{control_ip}:{control_port}"
+    auth_header = f"Bearer {control_token}"
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
     # Configure and test model
-    logging.debug("="*20 + " LOADING BASE MODEL (HIGH PRECISION) " + "="*20)
-    logging.debug(f"Using model: {model_str}")
+    logging.debug(f"using model: {model_str}")
     model = AutoModelForCausalLM.from_pretrained(
         model_str,
         torch_dtype=torch.bfloat16,
         device_map="auto"
     )
     tokenizer = AutoTokenizer.from_pretrained(model_str)
-
-    logging.debug("="*20 + " MODEL LOADED " + "="*20)
     generation_config = GenerationConfig(
         max_new_tokens=250,
         pad_token_id=tokenizer.eos_token_id,
         do_sample=True
     )
-    logging.debug("="*20 + " EXECUTING PROMPT " + "="*20)
 
-    current_task = "Jam the network at 1.5 GHz"
-    prompts = base_prompt + current_task
-    inputs = tokenizer(prompts, return_tensors="pt", padding=True, truncation=True).to(model.device)
-    output_tokens = model.generate(**inputs, generation_config=generation_config)
-    response_str = tokenizer.batch_decode(output_tokens, skip_special_tokens=True)
-    logging.info(f"Response from model: {response_str}")
+    base_prompt = Config.options.get("base_prompt", None)
+    if not base_prompt:
+        raise RuntimeError(f"base prompt not provided in {Config.filename}")
 
-    # Setup control API info
-    control_url = f"https://{control_ip}:{control_port}"
-    auth_header = f"Bearer {control_token}"
+    jammer_prompt = Config.options.get("jammer_prompt", None)
+    if not jammer_prompt:
+        raise RuntimeError(f"jammer prompt not provided in {Config.filename}")
 
-    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+    sniffer_prompt = Config.options.get("sniffer_prompt", None)
+    if not sniffer_prompt:
+        raise RuntimeError(f"sniffer prompt not provided in {Config.filename}")
+
+    rtue_prompt = Config.options.get("rtue_prompt", None)
+    if not rtue_prompt:
+        raise RuntimeError(f"rtue prompt not provided in {Config.filename}")
+
+    user_prompt = Config.options.get("user_prompt", None)
+    if not user_prompt:
+        raise RuntimeError(f"user prompt not provided in {Config.filename}")
 
 
     while True:
@@ -257,153 +261,11 @@ if __name__ == '__main__':
         # 2. verify the information and config (if fails go back to 1)
         # 3. send the message to controller endpoint
         # 4. prompt model again with message error or success
-        json_payload = {
-            "id": "llm_worker_1",
-            "type": "llm_worker",
-        }
-        get_process_logs(control_url, auth_header, json_payload)
         time.sleep(1)
+        all_prompt = base_prompt + jammer_prompt + sniffer_prompt + rtue_prompt + user_prompt
+        prompts = [all_prompt]
+        inputs = tokenizer(prompts, return_tensors="pt", padding=True, truncation=True).to(model.device)
+        output_tokens = model.generate(**inputs, generation_config=generation_config)
+        response_str = tokenizer.batch_decode(output_tokens, skip_special_tokens=True)
+        logging.info(response_str[-1])
 
-        list_processes(control_url, auth_header)
-        time.sleep(1)
-        json_payload = {
-            "id": "rtue_uhd_1",
-            "type": "rtue",
-            "config_str": """
-    [rf]
-    freq_offset = 0
-    tx_gain = 50
-    rx_gain = 40
-    srate = 23.04e6
-    nof_antennas = 1
-
-    device_name = uhd
-    device_args = clock=internal
-    time_adv_nsamples = 300
-
-    [rat.eutra]
-    dl_earfcn = 2850
-    nof_carriers = 0
-
-    [rat.nr]
-    bands = 3
-    nof_carriers = 1
-    max_nof_prb = 106
-    nof_prb = 106
-
-    [pcap]
-    enable = none
-    mac_filename = /tmp/ue_mac.pcap
-    mac_nr_filename = /tmp/ue_mac_nr.pcap
-    nas_filename = /tmp/ue_nas.pcap
-
-    [log]
-    all_level = info
-    phy_lib_level = none
-    all_hex_limit = 32
-    filename = /tmp/ue.log
-    file_max_size = -1
-
-    [usim]
-    mode = soft
-    algo = milenage
-    opc  = 63BFA50EE6523365FF14C1F45F88737D
-    k    = 00112233445566778899aabbccddeeff
-    imsi = 001010123456780
-    imei = 353490069873319
-
-    [rrc]
-    release = 15
-    ue_category = 4
-
-    [nas]
-    apn = srsapn
-    apn_protocol = ipv4
-
-    [gw]
-#netns = ue1
-#ip_devname = tun_srsue
-#ip_netmask = 255.255.255.0
-
-    [gui]
-    enable = false
-            """,
-            "rf": {
-                "type": "b200",
-                "images_dir": "/usr/share/uhd/images/",
-            }
-        }
-        start_process(control_url, auth_header, json_payload)
-        list_processes(control_url, auth_header)
-        time.sleep(1)
-
-        json_payload = {
-            "id": "jammer_uhd_1",
-            "type": "jammer",
-            "config_str": """
-    amplitude: 0.7
-    amplitude_width: 0.05
-    center_frequency: 1.842e9
-    bandwidth: 80e6
-    initial_phase: 0
-    sampling_freq: 40e6
-    num_samples: 20000
-    output_iq_file: "output.fc32"
-    output_csv_file: "output.csv"
-    write_iq: false
-    write_csv: true
-    device_args: "type=b200"
-    tx_gain: 70
-            """,
-            "rf": {
-                "type": "b200",
-                "images_dir": "/usr/share/uhd/images/",
-            }
-        }
-
-        start_process(control_url, auth_header, json_payload)
-        list_processes(control_url, auth_header)
-        time.sleep(1)
-
-        json_payload = {
-            "id": "sniffer_uhd_1",
-            "type": "sniffer",
-            "config_str": """
-    [sniffer]
-    file_path = "/home/oran-testbed/5g-sniffer/iq_1842MHz_pdcch_traffic.fc32"
-    sample_rate = 23040000
-    frequency = 1842500000
-    nid_1 = 1
-    ssb_numerology = 0
-#rf_args = "type=b200,master_clock_rate=23.04e6"
-
-
-    [[pdcch]]
-    coreset_id = 1
-    subcarrier_offset = 426
-    num_prbs = 30
-    numerology = 0
-    dci_sizes_list = [41]
-    scrambling_id_start = 1
-    scrambling_id_end = 10
-    rnti_start = 17921
-    rnti_end = 17930
-    interleaving_pattern = "non-interleaved"
-    coreset_duration = 1
-    AL_corr_thresholds = [1, 0.5, 0.5, 1, 1]
-    num_candidates_per_AL = [0, 4, 4, 0, 0]
-            """,
-            "rf": {
-                "type": "b200",
-                "images_dir": "/usr/share/uhd/images/",
-            }
-        }
-
-
-        start_process(control_url, auth_header, json_payload)
-        list_processes(control_url, auth_header)
-        time.sleep(1)
-
-        stop_process(control_url, auth_header, "rtue_uhd_1")
-        list_processes(control_url, auth_header)
-        time.sleep(10000)
