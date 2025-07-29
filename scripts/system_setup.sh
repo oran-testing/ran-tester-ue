fda4a1b35a6eceafa2a306d5620b60d8612b9c75
Charles Ueltschey <chaseuelt@gmail.com>
2025-07-29 13:20:19 -0500

    script push

--- scripts/system_setup.sh
+++ scripts/system_setup.sh
@@ -9,27 +9,27 @@ echo "Configuring with project root: $PROJECT_ROOT_DIR"
 read -p "Are you sure you want to proceed? (y/N): " confirm
 
 if [[ "$confirm" != "y" && "$confirm" != "Y" ]]; then
-	echo "Aborting cleanup."
-	exit 1
+  echo "Aborting cleanup."
+  exit 0
 fi
 
 sed -i "s|<rtusystem-dir>|${PROJECT_ROOT_DIR}|g" $PROJECT_ROOT_DIR/.env
 
 if ! command -v docker &>/dev/null; then
-	echo "Docker is not installed. Installing now..."
+  echo "Docker is not installed. Installing now..."
 
-	# Update package list
-	sudo apt update
+  # Update package list
+  sudo apt update
 
-	# Install Docker (Debian/Ubuntu)
-	sudo apt install -y docker.io
+  # Install Docker (Debian/Ubuntu)
+  sudo apt install -y docker.io
 
-	# Enable Docker service
-	sudo systemctl start docker
-	sudo systemctl enable docker
+  # Enable Docker service
+  sudo systemctl start docker
+  sudo systemctl enable docker
 
-	echo "Docker installation completed."
+  echo "Docker installation completed."
 else
-	echo "Docker is already installed!"
-	docker --version
+  echo "Docker is already installed!"
+  docker --version
 fi
